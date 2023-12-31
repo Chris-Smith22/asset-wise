aaf71f1c74e49dc1a6f5ad2e6e5ab8b7abbba7a3
Christopher Smith <chris.cookiesdt@icloud.com>
2023-12-31 10:22:58 -0500

    More yfinance practice, started main.py -> retrieve user data

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 print("hello world")
+'''
+Main.py:
+Will ask user for portfolio info:
+    either inputting manually, or
+    through a .csv file (check that merrill can do this) 
+
+    User should provide list of tickers held, and since when
+
+    if .csv file, read data on a separate read_data file/
+
+    Then with that data call on compute ticker asset info on separate file for each ticker
+
+    then finally produce summary of data (format_data.py)
+
+    and plot data (plot.py)
+
+'''
+
+
+
+def retrieve_tickers():
+    tickers_str = input("Please enter a comma separated list of tickers: \n").replace(' ', '')
+    print(tickers_str)
+    print(tickers_str.split(','))
+
+if __name__ =='__main__' :
+    retrieve_tickers()  
--- practice.py
+++ practice.py
@@ -1,10 +1,15 @@
 import yfinance as yf
 
 spy = yf.Ticker("SPY")
-spy_historical = spy.history(period="1d", interval="1d")
+spy_historical = spy.history(interval="1d", start="2023-05-01", end="2023-12-31")
 
+mkt_prices = spy_historical["Close"]
 
+'''
+print(mkt_prices)
+print(mkt_prices.iloc[-1])
+'''
 
-print(spy_historical)
-
-
+cap_gains = round(mkt_prices.iloc[-1] - mkt_prices.iloc[0],2)
+cg_rate = round(cap_gains/mkt_prices.iloc[0],4)*100
+print(f"Capital Gains: ${cap_gains}, Change: {cg_rate}%")
